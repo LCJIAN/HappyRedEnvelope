c7010618647ca3bbfdd665585c933e28edf9ad21
LCJIAN <lcjian89@gmail.com>
2017-06-16 16:04:07 +0800

    init

---
+++ app/src/main/java/com/lcjian/happyredenvelope/main/ExploreFragment.java
@@ -0,0 +1,20 @@
+package com.lcjian.happyredenvelope.main;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lcjian.happyredenvelope.R;
+
+public class ExploreFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_explore, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/lcjian/happyredenvelope/main/MainActivity.java
@@ -0,0 +1,17 @@
+package com.lcjian.happyredenvelope.main;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.lcjian.happyredenvelope.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.fl_main_fragment_container, new ExploreFragment()).commit();
+    }
+}
